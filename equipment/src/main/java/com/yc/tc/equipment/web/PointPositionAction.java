add2685f212153bf28c64a72621209f8a93c7fea
OrzFeel00 <632360579@qq.com>
2021-10-28 10:37:13 +0800

    引入layui2.56并且测试表单

--- equipment/src/main/java/com/yc/tc/equipment/web/PointPositionAction.java
+++ equipment/src/main/java/com/yc/tc/equipment/web/PointPositionAction.java
@@ -3,6 +3,7 @@ package com.yc.tc.equipment.web;
 import javax.annotation.Resource;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import com.yc.tc.equipment.biz.PointPositionBiz;
 
@@ -12,5 +13,13 @@ public class PointPositionAction {
 	@Resource
 	private  PointPositionBiz Pop;
 	
+	
+
+	//去首登录页面
+			@GetMapping("inPoint.html")
+			public String touindex() {
+				return "admin/inPoint";
+			}
+
 
 }
